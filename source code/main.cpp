ab0edeef465bccf3d7e378e01e76d190d0adccb5
javierrayhan <javrayhan2006@gmail.com>
2025-07-23 12:22:13 +0700

    Adding RGB LED

--- source code/main.cpp
+++ source code/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#include <Adafruit_NeoPixel.h>
+
 // Include liblary
 #include "button_input.h"
 #include "pixel_read.h"
@@ -7,16 +9,48 @@
 
 const int analogPin = 36;   // ADC1_CHANNEL_0
 const int shutterPin = 32;  // SHUTTER PIN 32
+const int RGBled = 27;
+
+void colorWipe(uint32_t color, int wait);
+
+Adafruit_NeoPixel strip(1, RGBled, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(115200);             // Serial init
-  setupPixel();                     // Pixel pin init
 
-  analogSetAttenuation(ADC_11db);   // Analog 3.3v
+  // WS2812B init
+  strip.begin();
+  strip.show();
+  strip.setBrightness(50);
+  colorWipe(strip.Color(0, 255, 0), 100);
+  colorWipe(strip.Color(0, 0, 0), 50);
 
-  initButton(shutterPin);           // Shutter pin init
+  // Pixel pin init
+  setupPixel();                     
+  colorWipe(strip.Color(0, 255, 0), 100);
+  colorWipe(strip.Color(0, 0, 0), 50);
 
-  setupSPI_SD();                    // SPI sdcard init
+  // Analog 3.3v
+  analogSetAttenuation(ADC_11db);
+  colorWipe(strip.Color(0, 255, 0), 100);
+  colorWipe(strip.Color(0, 0, 0), 50);
+
+  // Shutter pin init
+  initButton(shutterPin);           
+  colorWipe(strip.Color(0, 255, 0), 100);
+  colorWipe(strip.Color(0, 0, 0), 50);
+
+   // SPI sdcard init
+  if (setupSPI_SD()){
+    colorWipe(strip.Color(0, 255, 0), 100);
+  }
+
+  else if (!setupSPI_SD()){
+    colorWipe(strip.Color(255, 0, 0), 100);
+  }    
+
+  colorWipe(strip.Color(0, 0, 0), 50);
+
 }
 
 void loop() {
@@ -24,12 +58,33 @@ void loop() {
 
   if(isButtonPressedShort()){
     readValue(analogPin);
+
   } 
+
   else if (isButtonPressedMedium()) {
-    
+    readValue(analogPin);
+
+    if(saveBMPToSD(result8bit, 8, 8)){
+      colorWipe(strip.Color(0, 255, 0), 100);
+      colorWipe(strip.Color(0, 0, 0), 50);
+    }
+    else if (!saveBMPToSD(result8bit, 8, 8)){
+      colorWipe(strip.Color(255, 0, 0), 100);
+      colorWipe(strip.Color(0, 0, 0), 50);
+    }
   } 
+
   else if (isButtonPressedLong()) {
     
   }
 
 }
+
+void colorWipe(uint32_t color, int wait) {
+  for(int i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, color);
+    strip.show();
+    delay(wait);
+  }
+}
+
